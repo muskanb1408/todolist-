4376d5ffa66ccccfc8ff05e9f6c6193c6c8b59aa
muskanb1408 <70267379+muskanb1408@users.noreply.github.com>
2021-07-29 23:47:49 +0530

    Updates

---
+++ precache-manifest.1e11f6d61582c5ccc6dcb8507bf1d7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6b497d218552e9a7df09f8fe0cd669",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "c249c814c8220309acfa",
+    "url": "/todolist-react/static/css/main.11870ee5.chunk.css"
+  },
+  {
+    "revision": "3baa5fcdec19a3fbd8e5",
+    "url": "/todolist-react/static/js/2.db352911.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.db352911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c249c814c8220309acfa",
+    "url": "/todolist-react/static/js/main.a1aba722.chunk.js"
+  },
+  {
+    "revision": "0eed23ab9c1895acb675",
+    "url": "/todolist-react/static/js/runtime-main.56464843.js"
+  }
+]);
